ab50cc9bd00396512712bda79bbde50364034ecd
Ophir Mizrahi <102543072+OphirMiz@users.noreply.github.com>
2026-01-25 22:42:32 +0200

    Update PixelManager.h

--- ESP32/PixelManager.h
+++ ESP32/PixelManager.h
@@ -7,7 +7,7 @@ void neoOff();
 void neoSetColorAll(uint8_t r, uint8_t g, uint8_t b);
 void initPixels();
 
-// ✅ UPDATED: now also takes wifiConnected
+
 void updateNeoPixel(bool containerEmpty, bool wifiConnected);
 
 #endif
